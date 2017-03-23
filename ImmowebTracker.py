f89e00dbfb33b145e2aa58b8f2b7860feb9cd7c4
Adrien Faucon <adrien.faucon@gmail.com>
2017-03-23 20:52:34 +0100

    Second commit

--- ImmowebTracker.py
+++ ImmowebTracker.py
@@ -1,5 +1,5 @@
 debug = False
-
+temp_dir = "c:\\Users\\adrie\\Data\\temp\\"
 
 def print_if_debug_is_set(str):
     if debug:
@@ -16,14 +16,14 @@ def get_list_of_id(url):
     from bs4 import BeautifulSoup
     soup = BeautifulSoup(html_doc, 'html.parser')
 
-    import time
-    filename = time.strftime("%Y%m%d-%H%M%S_orig") + ".log"
-    with open("C:\\Data\\temp5\\" + filename, "wb") as myfile:
-        myfile.write(html_doc)
-
-    filename = time.strftime("%Y%m%d-%H%M%S_soup2") + ".log"
-    with open("C:\\Data\\temp5\\" + filename, "w", encoding='utf-8') as myfile:
-        myfile.write(soup.prettify())
+    # import time
+    # filename = time.strftime("%Y%m%d-%H%M%S_orig") + ".log"
+    # with open(temp_dir + filename, "wb") as myfile:
+    #     myfile.write(html_doc)
+    # 
+    # filename = time.strftime("%Y%m%d-%H%M%S_soup2") + ".log"
+    # with open(temp_dir + filename, "w", encoding='utf-8') as myfile:
+    #     myfile.write(soup.prettify())
     
     result = []
     for item in soup.find_all():
@@ -74,7 +74,7 @@ def get_new_real_estate_ad():
     print_if_debug_is_set("In get_new_real_estate_ad:")
     
     # Step 0 : Local variable
-    file_path = "C:\\Data\\temp5\\list_of_id.txt"
+    file_path = temp_dir + "list_of_id.txt"
     
     # Step 1 : Recovered the list of id from Immoweb
     url = 'http://www.immoweb.be/fr/recherche/maison/a-vendre/hainaut?XORDERBY1=datemodification'
@@ -94,12 +94,26 @@ def get_new_real_estate_ad():
     
     
 def send_to_pushbullet(new_list_of_id):
+    print_if_debug_is_set("\n--")
+    print_if_debug_is_set("In send_to_pushbullet:")
     from pushbullet import Pushbullet
     try:
         pb = Pushbullet('o.mjx5s4pc2ScCZFUoqcA8WH307cIdeXfB')
+        print_if_debug_is_set("Adrien Faucon devices: " + str(pb.devices))
+        my_smartphone = pb.get_device('LGE Nexus 5')
+        
+        if len(new_list_of_id) == 0:
+            print_if_debug_is_set("Nothing to send")
+        else:
+            for id in new_list_of_id:
+                url = "http://www.immoweb.be/fr/Global.Estate.cfm?IdBien=" + str(id)
+                push = pb.push_note("New house!", url, device=my_smartphone)
     except Exception as e:
         print(str(e))
-        print(repr(e.__context__))
+    
+    
+def full_program():
+    send_to_pushbullet(get_new_real_estate_ad())
     
     
 if __name__ == '__main__':
@@ -110,7 +124,8 @@ if __name__ == '__main__':
              load_list_of_id,
              find_new_id_in_current_list,
              get_new_real_estate_ad,
-             send_to_pushbullet]
+             send_to_pushbullet,
+             full_program]
     
     if debug == True:
         cnt = 0
@@ -125,11 +140,11 @@ if __name__ == '__main__':
         
         if tests[selection] == store_list_of_id:
             list_of_id = [6910224,6910212,6910210,6910162,6910159,6910144,6910141,6910110,6910096,6910094,6910093,6910092,6800346,6910072,6910042,6910040,6651983,6910025,6804106,6909998,6909997,6909996,6866227,6909938,6796249,6909925,6906629,6909877,6506592,6834357]
-            file_path = "C:\\Data\\temp5\\list_of_id.txt"
+            file_path = temp_dir + "list_of_id.txt"
             store_list_of_id(list_of_id, file_path)
         
         if tests[selection] == load_list_of_id:
-            file_path = "C:\\Data\\temp5\\list_of_id.txt"
+            file_path = temp_dir + "list_of_id.txt"
             list_of_id = load_list_of_id(file_path)
             
         if tests[selection] == find_new_id_in_current_list:
@@ -141,7 +156,11 @@ if __name__ == '__main__':
             new_list_of_id = get_new_real_estate_ad()
             
         if tests[selection] == send_to_pushbullet:
-            new_list_of_id = [1234567,6666666]
+            new_list_of_id = [6910450,6910427]
             send_to_pushbullet(new_list_of_id)
+        
+        if tests[selection] == full_program:
+            full_program()
+            
     else:
-        get_new_real_estate_ad()
+        full_program()
